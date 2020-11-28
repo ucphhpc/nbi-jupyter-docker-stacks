8e2932179afd2d594d447d94913f92343f02df59
Jonas Bardino <bardino@nbi.ku.dk>
2020-11-28 10:59:06 +0100

    statistics-notebook: added sympy for py2+3 and updated iminuit to latest version for py3 as requested by ERDA user [ci][deploy]. Roll back jupyter-leaflet version to 0.12.4 for py2 as later versions introduce subtle py3-only constructs breaking docker test. Similarly restrict iminuit to last known py2-compatible version 1.3.8 in the py2 env. Inline fix for missing encoding header in ipyleaflet leaflet.py which also broke docker test. Switch to the new jupyterlab_pip in Dockerfile.test now that basic pip is no longer available there.

--- statistics-notebook/tests/test.sh
+++ statistics-notebook/tests/test.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+#
+# Launched as the last action of Dockerfile.test after it has installed
+# required dependencies like pytest and pytest-xdist plugin with
+# pip install -r requirements.txt
+# from here.
+
 pytest -n 4 --durations=0
