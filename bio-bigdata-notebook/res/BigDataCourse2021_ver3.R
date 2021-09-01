2f4fd59332f34831c6d06a2feef3ce3d4e22af61
Rasmus Munk <munk1@live.dk>
2021-09-01 11:24:51 +0200

    Update the r-notebook to use the new Ubuntu 5.7 security repo for installing libssl, also update the bio-bigdata-notebook to inherit directly from the r-notebook to avoid package conflicts with the bio-notebook

---
+++ bio-bigdata-notebook/res/BigDataCourse2021_ver3.R
@@ -0,0 +1,34 @@
+## Installing CRAN packages
+install.packages(c(
+    "hexbin",
+    "ggplot2",
+    "pheatmap",
+    "plyr",
+    "FactoMineR",
+    "factoextra",
+    "dplyr",
+    "tidyr",
+    "magick",
+    "magrittr",
+    "gridExtra",
+    "corpcor",
+    "rmarkdown",
+    "tinytex"
+), repos='http://mirrors.dotsrc.org/cran/')
+tinytex::install_tinytex()
+
+# Installing packages from Bioconductor
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager", repos='http://mirrors.dotsrc.org/cran/')
+
+library(BiocManager)
+
+BiocManager::install("DESeq2", update = TRUE, ask = FALSE)
+BiocManager::install("dada2", update = TRUE, ask = FALSE)
+BiocManager::install("vsn", update = TRUE, ask = FALSE)
+BiocManager::install("multtest", update = TRUE, ask = FALSE)
+BiocManager::install("xcms", update = TRUE, ask = FALSE)
+BiocManager::install("BiocParallel", update = TRUE, ask = FALSE)
+BiocManager::install("CAMERA", update = TRUE, ask = FALSE)
+BiocManager::install("pcaMethods",update = TRUE, ask = FALSE)
+BiocManager::install("apeglm", update = TRUE, ask = FALSE)
