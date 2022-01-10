11fd2f71cb4bc25bc61aa453da48feeaccecfd17
Rasmus Munk <munk1@live.dk>
2022-01-10 13:00:59 +0100

    Rename to notebook_images

--- deploy_notebook.sh
+++ deploy_notebook.sh
@@ -2,9 +2,6 @@
 NOTEBOOK=$1
 TAG=$2
 
-# Ensure that the TAG parameter is not more than 12 chars
-TAG=${TAG}
-
 if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
     echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
 fi
