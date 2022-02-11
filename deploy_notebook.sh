0d814f383572b2565ce36451f2eda76df341c7ba
Rasmus Munk <munk1@live.dk>
2022-02-11 14:36:22 +0000

    Add the slurm-notebook 21.08.0 and 20.02.07 to the gocd config

--- deploy_notebook.sh
+++ deploy_notebook.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 NOTEBOOK=$1
-TAG=$2
+ARGS=$2
+TAG=$3
 
 if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
     echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
 fi
 
-make build/${NOTEBOOK} TAG=${TAG}
-make push/${NOTEBOOK} TAG=${TAG}
+make build/${NOTEBOOK} TAG=${TAG} ARGS=${ARGS}
+make push/${NOTEBOOK} TAG=${TAG} ARGS=${ARGS}
