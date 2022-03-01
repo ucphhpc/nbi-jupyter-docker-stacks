9cff853b561197e56b842e2f501c4748e80f2c83
Rasmus Munk <munk1@live.dk>
2022-03-01 10:28:24 +0100

    Fix the EXTRA_TAG deploy check

--- deploy_notebook.sh
+++ deploy_notebook.sh
@@ -8,13 +8,13 @@ if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
     echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
 fi
 
-make build/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
-make push/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
+#make build/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
+#make push/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
 
 # If the EXTRA_TAG is set.
 # Link it to the original tag and push that version as well
-echo "Extra tag ${EXTRA_TAG}"
-if [[ -z ${EXTRA_TAG+x} ]]; then
+if [[ -n ${EXTRA_TAG} ]]; then
+    echo 'Hello'
     ln -s ${NOTEBOOK}/Dockerfile.${TAG} ${NOTEBOOK}/Dockerfile.${EXTRA_TAG}
 
     make build/${NOTEBOOK} TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
