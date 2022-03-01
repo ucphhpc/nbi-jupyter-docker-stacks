41761d34428a9ddceffad52745c7a395c5f4ccb0
Rasmus Munk <munk1@live.dk>
2022-03-01 09:49:45 +0100

    Add the option for the deploy_notebook script to deploy an extra tag

--- deploy_notebook.sh
+++ deploy_notebook.sh
@@ -2,6 +2,7 @@
 NOTEBOOK=$1
 BUILD_ARGS=$2
 TAG=$3
+EXTRA_TAG=$4
 
 if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
     echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
@@ -9,3 +10,12 @@ fi
 
 make build/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
 make push/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
+
+# If the EXTRA_TAG is set.
+# Link it to the original tag and push that version as well
+if [[ -z $EXTRA_TAG ]]; then
+    ln -s ${NOTEBOOK}/Dockerfile.${TAG} ${NOTEBOOK}/Dockerfile.${EXTRA_TAG}
+
+    make build/${NOTEBOOK} TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
+    make push/${NOTEBOOK} TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
+fi
