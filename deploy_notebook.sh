3f6bef9f40bc19fdcb7ab3f0b6a8a47a25b6deae
Rasmus Munk <munk1@live.dk>
2022-03-01 18:19:51 +0100

    Try debug env expand

--- deploy_notebook.sh
+++ deploy_notebook.sh
@@ -8,13 +8,14 @@ if [[ -n ${DOCKERHUB_USERNAME} ]] && [[ -n ${DOCKERHUB_PASSWORD} ]]; then
     echo "${DOCKERHUB_PASSWORD}" | docker login -u ${DOCKERHUB_USERNAME} --password-stdin
 fi
 
-#make build/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
-#make push/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
+make build/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
+make push/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
 
 # If the EXTRA_TAG is set.
 # Link it to the original tag and push that version as well
 if [[ -n ${EXTRA_TAG} ]]; then
-    echo 'Hello'
+    echo "Extra tag expanded: ${!EXTRA_TAG}"
+
     ln -s ${NOTEBOOK}/Dockerfile.${TAG} ${NOTEBOOK}/Dockerfile.${EXTRA_TAG}
 
     make build/${NOTEBOOK} TAG=${EXTRA_TAG} ARGS=${BUILD_ARGS}
