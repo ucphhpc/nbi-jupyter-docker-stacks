1219fb1c4c7b2c9a5ad5d14e5f1da5b807cfa3c7
Rasmus Munk <munk1@live.dk>
2022-03-02 17:54:01 +0000

    Try assigning the GOCD env var before accessing it

--- deploy_notebook.sh
+++ deploy_notebook.sh
@@ -13,12 +13,12 @@ make push/${NOTEBOOK} TAG=${TAG} ARGS=${BUILD_ARGS}
 
 # If the EXTRA_TAG is set.
 # Link it to the original tag and push that version as well
-echo "Pre test EXTRA_TAG expanded: ${!EXTRA_TAG}"
-# Test which envvars are available in GOCD
-printenv
+echo "Pre test EXTRA_TAG: ${EXTRA_TAG}"
+extra_tag_var_name=${EXTRA_TAG}
 
 if [[ -n ${EXTRA_TAG} ]]; then
-    echo "Extra tag expanded: ${!EXTRA_TAG}"
+    echo "Extra tag: ${extra_tag_var_name}"
+    echo "extra tag expanded: ${!extra_tag_var_name}"
 
     ln -s ${NOTEBOOK}/Dockerfile.${TAG} ${NOTEBOOK}/Dockerfile.${EXTRA_TAG}
 
