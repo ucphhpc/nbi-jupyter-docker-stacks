deba3a0e4e1a53bd756bb2f0bbbaa56545a14183
Rasmus Munk <rasmunk@users.noreply.github.com>
2025-04-02 16:43:06 +0200

    Update the r-notebook to use keras3 and in tandem upgrade the underlying tensorflow to 2.19.0

--- r-notebook/res/keras_package.R
+++ r-notebook/res/keras_package.R
@@ -1,6 +1,13 @@
 
 # CRAN packages
-install.packages(c("keras"), repos="http://mirrors.dotsrc.org/cran/")
+install.packages("keras3", repos="https://mirrors.dotsrc.org/cran/")
+library(keras3)
 
-library(keras)
-install_keras(method='conda', version='2.13.0')
+# install_keras uses reticulate to discover the Python environment
+# Therefore python_version can also be an absolute path to a python binary
+# https://github.com/rstudio/reticulate/blob/56ea74e810f3b2fca3b67a973414624933e09a37/R/virtualenv.R#L503
+install_keras(
+    python_version = Sys.getenv("RETICULATE_PYTHON"),
+    backend=c("tensorflow", "jax"),
+    restart_session=FALSE
+)
